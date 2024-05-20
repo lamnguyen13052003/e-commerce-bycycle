1394040f13771f0da180c218bc62327d36cfcb87
lamnguyen <kiminonawa1305@gmail.com>
2024-05-20 17:05:31 +0700

    cấu trúc cây thư mục

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './configs/reportWebVitals';
 import {DevSupport} from "@react-buddy/ide-toolbox";
 import {ComponentPreviews, useInitial} from "./dev";
 
