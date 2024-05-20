79195d5f83ddab5b71f1f7aef16b1207d97fc8e8
lamnguyen <kiminonawa1305@gmail.com>
2024-05-21 06:29:28 +0700

    Hoàn thành cơ bản header và footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './assets/images/logo.svg';
 import './styles/App.css';
+import {Route, Routes} from "react-router";
+import Main from "./pages/Main";
+import Home from "./pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap-submenu/dist/index.css";
+import "bootstrap";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Routes>
+            <Route path="/" element={<Main/>}>
+                <Route index element={<Home/>}/>
+            </Route>
+        </Routes>
     );
 }
 
