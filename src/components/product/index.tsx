0a2f8088d76397ac3b0357c3a7f250b89372215c
lamhongphong <21130473@st.hcmuaf.edu.vn>
2024-05-24 10:23:43 +0700

    ui: finish square product

--- src/components/product/index.tsx
+++ src/components/product/index.tsx
@@ -1,7 +1,10 @@
-import {Box, Stack} from "@mui/material";
+import {Box, Button, Stack} from "@mui/material";
 import styles from './scss/Product.module.css';
 import BoltIcon from '@mui/icons-material/Bolt';
 import React from "react";
+import {Image} from "react-bootstrap";
+import ImageCycleBike from '../../assets/images/xe-dap-dua-calli-r6-4.jpg'
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 interface productProps{
     isSale: boolean,
     discount: number,
@@ -12,8 +15,29 @@ interface productProps{
 }
 function Product(props:productProps){
     return (
-        <Box className={'position-relative mx-2 shadow-sm rounded-4 border border-1'} sx={{width:'306px', height:'200px'}}>
-            <LabelDiscount discount={props.discount}/>
+        <Box className={' mx-2 shadow-lg rounded-4 overflow-hidden'} sx={{width:'306px', height: '376px'}}>
+            <Box className={'position-relative'} sx={{height:'218px'}}>
+                <LabelDiscount discount={props.discount}/>
+                <Box className={'object-fit-cover position-absolute top-0 start-0 z-0'} sx={{width: '306px', height:'218px'}}>
+                    <Image className={`${styles.image_hover}`} src={ImageCycleBike} alt={props.name} style={{width: '100%'}}/>
+                </Box>
+            </Box>
+
+            <Box className={'p-2 text-center fw-bold '}>
+                <Stack className={'d-flex align-items-center justify-content-center'} direction={"column"} spacing={1}>
+                    <Box className={`${styles.text_line_clamp}`}>
+                        <p>{props.name}</p>
+                    </Box>
+
+                    <Stack className={'p-2 '} direction={"row"} spacing={3}>
+                        <span className={'text-body-secondary    fw-normal'}><del>{props.price}<sup>đ</sup></del></span>
+                        <span className={'fs-4 text-danger'}>{props.discountPrice}<sup>đ</sup></span>
+                    </Stack>
+
+                    <Button className={'text-uppercase'} variant={"contained"} color={"info"} startIcon={<ShoppingCartIcon/>}
+                        style={{width: '148px', height: '28px'}}> Mua hàng</Button>
+                </Stack>
+            </Box>
         </Box>
     )
 }
