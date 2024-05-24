8584cc579d463d40d254b973af44706dbe719602
lamhongphong <21130473@st.hcmuaf.edu.vn>
2024-05-24 10:28:55 +0700

    style: comment

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ function Home() {
     return (
         <>
             <div>Trang chủ</div>
+            {/*test thoi*/}
         <Product isSale={true} discount={3} image={{}} name={'Xe Đạp Đua Twitter Smile – Khung Nhôm | Tay Đề Lắc | Retrospec | hahahahahahaha'} price={3000000} discountPrice={2500000}/>
         </>
     );
