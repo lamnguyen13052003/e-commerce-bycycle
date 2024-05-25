5b7f74943b40be7502bdc8f32c7d2a013e03bb40
hntqung2210 <21130152@st.hcmuaf.edu.vn>
2024-05-26 03:52:38 +0700

    push thieu page Cart

---
+++ src/pages/Cart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CartItem from "../components/cart";
+
+function Cart() {
+    return (
+        <>
+            <CartItem id={1} title={"Xe Đạp Trẻ Em 12 Inch GH Bike [GIÁ RẺ] - Xanh lá"} price={890000} qty={1}/>
+        </>
+    );
+}
+
+export default Cart;
