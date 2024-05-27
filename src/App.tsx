c802804c18b770dc385d068fac286f500cb09fc3
lamhongphong <21130473@st.hcmuaf.edu.vn>
2024-05-27 13:35:44 +0700

    ui: working 1/4 task

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import Home from "./pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-bootstrap-submenu/dist/index.css";
 import Cart from "./pages/Cart";
+import Products from "./pages/Products";
+
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Main/>}>
                 <Route index element={<Home/>}/>
-                <Route path={"/cart"} element={<Cart/>}>        </Route>
+                <Route path={"/cart"} element={<Cart/>}></Route>
+                <Route path={"/xe-dap-dua"} element={<Products/>} />
             </Route>
         </Routes>
     );
---
+++ src/pages/Products.tsx
@@ -0,0 +1,83 @@
+import {ProductProps} from "../components/product";
+import {Box, Breadcrumbs, FormControl, InputLabel, Select, SelectChangeEvent, Stack} from "@mui/material";
+import {Link} from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import React from "react";
+import {Container} from "react-bootstrap";
+import MenuItem from '@mui/material/MenuItem';
+
+function ProductPageInfo(props: PageData) {
+    return (
+        <>
+            <Box className={'p-3 d-lg-flex justify-content-between align-items-center'}>
+                <Box>
+                    <Breadcrumbs className={'fw-bold text-uppercase'} aria-label="breadcrumb">
+                        <Link color="inherit" to="/">
+                            Trang chủ
+                        </Link>
+                        <Typography color="text.primary">{props.nameCategory}</Typography>
+                    </Breadcrumbs>
+                </Box>
+
+                <Box className={'fw-bold'}>
+                    <Stack  direction={'row'} alignItems={'center'} alignContent={'center'} gap={1}>
+                        <Box>
+                            <span>Hiển thị tất cả x kết quả</span>
+                        </Box>
+                        <SelectSmallFilter/>
+                    </Stack>
+
+                </Box>
+            </Box>
+
+        </>
+    )
+}
+
+function SelectSmallFilter() {
+    const [filter, setFilter] = React.useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setFilter(event.target.value as string);
+    };
+
+    return (
+        <Box sx={{ minWidth: 120 }}>
+            <FormControl sx={{m: 1, minWidth: 220}} fullWidth>
+                <InputLabel id="select-small-label">Lọc</InputLabel>
+                <Select
+                    labelId="select-small-label"
+                    id="select-small"
+                    value={filter}
+                    label="Filter"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={0}>Mới nhất</MenuItem>
+                    <MenuItem value={1}>Theo giá: Tăng dần</MenuItem>
+                    <MenuItem value={2}>Theo giá: Giảm dần</MenuItem>
+                    <MenuItem value={3}>Theo mức độ phổ biến</MenuItem>
+                    <MenuItem value={4}>Theo điểm đánh giá</MenuItem>
+                </Select>
+            </FormControl>
+        </Box>
+
+    );
+}
+
+function Products() {
+    return (
+        <>
+            <Container>
+                <ProductPageInfo {...pageData}/>
+            </Container>
+
+        </>
+    )
+}
+
+interface PageData {
+    nameCategory: string
+}
+
+const pageData: PageData = {nameCategory: "Xe đạp đua"}
+export default Products;
